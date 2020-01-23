db83b8a3d7c9624333e28fadab0d1c469f5683ba
Osamah Mandawi <oamandawi@brandeis.edu>
2020-01-23 17:25:43 -0500

    disabled browser caching to allow css updates to show up for everone

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ import pyowm
 
 app = Flask(__name__)
 
+
+@app.after_request
+def set_response_headers(response):
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
 # main page
 @app.route("/")
 def home():
