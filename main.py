7fe3ef68ff26f118f383c44df732fb2d06c7a1e0
Osamah Mandawi <oamandawi@brandeis.edu>
2020-04-09 13:21:22 -0400

    added small exception to passdata

--- main.py
+++ main.py
@@ -206,13 +206,14 @@ def login():
         APP.logger.info("Searching for current user")
         curr_user = [user for user in towear_users if user.username == username][0]
         APP.logger.info("Current user is %s", (curr_user.my_id))
-        for passdata_block in passdata:
-            APP.logger.info("Decrypting password")
-            if sha256_crypt.verify(password, passdata_block):
-                session["user_id"] = curr_user.my_id
-                session["log"] = True
-                APP.logger.info("Successfully logged in")
-                return redirect(url_for("closet"))
+        if passdata:
+            for passdata_block in passdata:
+                APP.logger.info("Decrypting password")
+                if sha256_crypt.verify(password, passdata_block):
+                    session["user_id"] = curr_user.my_id
+                    session["log"] = True
+                    APP.logger.info("Successfully logged in")
+                    return redirect(url_for("closet"))
         APP.logger.info(
             "Login authentication complete.\nNo such user.\nRedirecting to login page..."
         )
